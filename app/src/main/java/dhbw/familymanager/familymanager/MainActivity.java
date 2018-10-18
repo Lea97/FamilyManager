2e4e3e1f632cd97d9d5cbef2b8864a305273c226
Lea97 <lea.brock1@web.de>
2018-10-18 13:57:36 +0200

    Add Firebase Connection

--- app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
@@ -1,13 +1,26 @@
 package dhbw.familymanager.familymanager;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
 }
