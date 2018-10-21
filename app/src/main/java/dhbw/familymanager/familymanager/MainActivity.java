c30e047e90f435ca9ee58327c3f03b733e8b1cf3
Lea97 <lea.brock1@web.de>
2018-10-21 14:59:10 +0200

    Add Login and SignUp View

--- app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
@@ -1,8 +1,11 @@
 package dhbw.familymanager.familymanager;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,11 +20,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
 
+        setContentView(R.layout.login_page);
 
-
+        Button button= (Button)findViewById(R.id.registrationButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.layout.login_page:
+                        startActivity(new Intent(this, SignInActivity.class));
+                        break;
+                }
+            }
+        });
 
     }
 
---
+++ app/src/main/java/dhbw/familymanager/familymanager/SignInActivity.java
@@ -0,0 +1,30 @@
+package dhbw.familymanager.familymanager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText email;
+    EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_page);
+
+
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.registrationButton:
+                break;
+
+
+        }
+    }
+}
