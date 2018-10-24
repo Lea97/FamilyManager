19b589d5bc14442581fa1a939df3111809dadaf7
Lea97 <lea.brock1@web.de>
2018-10-24 16:02:00 +0200

    Add register User function

--- app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
@@ -1,41 +1,28 @@
 package dhbw.familymanager.familymanager;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
-
-    private FirebaseAuth mAuth;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
-
         setContentView(R.layout.login_page);
 
-        Button button= (Button)findViewById(R.id.registrationButton);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()){
-                    case R.layout.login_page:
-                        startActivity(new Intent(getApplicationContext(), SignInActivity.class));
-                        break;
-                }
-            }
-        });
+        findViewById(R.id.registrationButton).setOnClickListener(this);
+        }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.registrationButton:
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                break;
+        }
     }
-
 }
---
+++ app/src/main/java/dhbw/familymanager/familymanager/SignUpActivity.java
@@ -0,0 +1,106 @@
+package dhbw.familymanager.familymanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText textFieldEmail;
+    EditText textFieldPassword;
+    EditText textFieldRepeatPassword;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+
+        textFieldEmail = findViewById(R.id.emailField);
+        textFieldPassword = findViewById(R.id.passwordField);
+        textFieldRepeatPassword = findViewById(R.id.repeatPasswordField);
+
+        findViewById(R.id.toLoginButton).setOnClickListener(this);
+        findViewById(R.id.loginButton).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void registerUser() {
+        String email = textFieldEmail.getText().toString().trim();
+        String password = textFieldPassword.getText().toString().trim();
+        String repeatPassword = textFieldRepeatPassword.getText().toString().trim();
+
+
+        if(email.isEmpty()){
+            textFieldEmail.setError("Email is required");
+            textFieldEmail.requestFocus();
+            return;
+        }
+
+        if(!(Patterns.EMAIL_ADDRESS.matcher(email).matches())){
+            textFieldEmail.setError("Enter a valid email");
+            textFieldEmail.requestFocus();
+            return;
+        }
+
+        if(password.length()<6){
+            textFieldPassword.setError("Minimum length of password should be 6");
+            textFieldPassword.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            textFieldPassword.setError("Password is required");
+            textFieldRepeatPassword.setError("Password is required");
+            textFieldPassword.requestFocus();
+            return;
+        }
+
+        if(!(password.equals(repeatPassword))){
+            textFieldPassword.setError("Password is required");
+            textFieldRepeatPassword.setError("Password is required");
+            textFieldPassword.requestFocus();
+            return;
+        }
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(getApplicationContext(), "User Registered Successful", Toast.LENGTH_SHORT).show();
+                        }
+                        if(task.isCanceled()){
+                            Toast.makeText(getApplicationContext(), "User could not Registered", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.toLoginButton:
+               startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                break;
+            case R.id.loginButton:
+                registerUser();
+                break;
+        }
+    }
+}
