21fc05a121e1a48a9ab19a7dad73224461dbaca7
Anastasia Schwed <nshmyreva@gmail.com>
2018-11-01 16:09:12 +0100

    added firebase ui

--- app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
@@ -1,28 +1,90 @@
 package dhbw.familymanager.familymanager;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity
 {
+    private static final int RC_SIGN_IN = 4711;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
-
-        findViewById(R.id.registrationButton).setOnClickListener(this);
-        }
+        setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
+    }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.registrationButton:
-                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
-                break;
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build());
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+
+        } else {
+            System.out.println("HalloHallo");
         }
+
     }
-}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }}
+
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.login_page);
+        //findViewById(R.id.registrationButton).setOnClickListener(this);
+       // }
+
+
+    //public void onClick(View view) {
+      //  switch (view.getId()) {
+        //    case R.id.registrationButton:
+          //      startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            //    break;
+        //}
+    //}
+//}
