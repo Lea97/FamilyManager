6da474ad247d954b7de2559f36afa1e5163d6ce0
Anastasia Schwed <nshmyreva@gmail.com>
2019-01-05 21:08:44 +0100

    added test events

--- app/src/main/java/dhbw/familymanager/familymanager/CalendarActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/CalendarActivity.java
@@ -17,35 +17,67 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class CalendarActivity extends AppCompatActivity {
     private WeekView mWeekView;
     private FloatingActionButton addEventButton;
+    private Random random = new Random();
+
+    private List<WeekViewEvent> events = createEvents();
+
+    private List<WeekViewEvent> createEvents() {
+        List<WeekViewEvent> result = new ArrayList<>();
+        Calendar now = new GregorianCalendar();
+        Calendar current = (Calendar) now.clone();
+        current.add(Calendar.DAY_OF_YEAR, -1);
+        current.set(Calendar.HOUR_OF_DAY, 0);
+        current.set(Calendar.MINUTE, 0);
+
+
+        for (int i = 1; i <= 12; i++) {
+            String title = "Termin " + i;
+            WeekViewEvent e = createEvent(current, title);
+            result.add(e);
+            current.add(Calendar.HOUR_OF_DAY, 5);
+        }
+
+        return result;
+    }
+
+    private WeekViewEvent createEvent(Calendar startTime, String title) {
+
+        Calendar startTimeCopy = (Calendar) startTime.clone();
+        Calendar endTime = (Calendar) startTime.clone();
+        endTime.add(Calendar.HOUR, 1);
+        List<WeekViewEvent> events = new ArrayList<>();
+        WeekViewEvent e = new WeekViewEvent(random.nextLong(), title, "Test location", startTimeCopy, endTime);
+        return e;
+
+    }
+
 
     MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
         @Override
-        public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-            // Populate the week view with some events.
-            Calendar now = new GregorianCalendar();
-            Calendar nowplusone = (Calendar) now.clone();
-            nowplusone.add(Calendar.HOUR, 1);
-            List<WeekViewEvent> events = new ArrayList<>();
-            if (newMonth == now.get(Calendar.MONTH)) {
-                WeekViewEvent e = new WeekViewEvent(4711, "Tralala", "Hopala", now, nowplusone);
-                events.add(e);
-
+        public List<WeekViewEvent> onMonthChange(int newYear, final int newMonth) {
+            ArrayList<WeekViewEvent> result = new ArrayList<>();
+            for (WeekViewEvent e : events) {
+                if (e.getStartTime().get(Calendar.MONTH) + 1 == newMonth && e.getStartTime().get(Calendar.YEAR)+1==newYear)
+                {
+                    result.add(e);
+                }
             }
-            return events;
+            return result;
         }
     };
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.calendar_menu, menu);
         return true;
     }
-
 
 
     @Override
@@ -71,11 +103,13 @@ public class CalendarActivity extends AppCompatActivity {
         mWeekView.showContextMenu();
 
 
-        addEventButton=findViewById(R.id.addEventButton);
+        addEventButton = findViewById(R.id.addEventButton);
 
 // The week view has infinite scrolling horizontally. We have to provide the events of a
 // month every time the month changes on the week view.
         mWeekView.setMonthChangeListener(mMonthChangeListener);
+
+
         addEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -85,13 +119,13 @@ public class CalendarActivity extends AppCompatActivity {
         });
 
 
-        }
-
-
-        //mWeekView.setOnEventClickListener(onEventClickListener);
-
-
     }
+
+
+    //mWeekView.setOnEventClickListener(onEventClickListener);
+
+
+}
 
 
 
