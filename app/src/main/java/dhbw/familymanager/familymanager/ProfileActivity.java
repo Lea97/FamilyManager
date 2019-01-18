4f4593a0ba6a8f08ff8b5fa5c62123ccce8ed311
Lea97 <lea.brock1@web.de>
2019-01-18 22:13:13 +0100

    Create Upload for a new profile picture and store it in Firestorage.

--- app/src/main/java/dhbw/familymanager/familymanager/ProfileActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/ProfileActivity.java
@@ -1,18 +1,28 @@
 package dhbw.familymanager.familymanager;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,33 +31,116 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.UUID;
 
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
+    private Button btnChoose;
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
+
     private FirebaseUser user;
     private Boolean editmode;
     private ImageView mImageView;
+    private static final int FILE_SELECT_CODE = 0;
+    private static final String TAG = null;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    String picturePath;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         setValues();
         setContentView(R.layout.profil);
         editmode = false;
         findViewById(R.id.changeProfilButton).setOnClickListener(this);
     }
 
+    private void showFileChooser() {
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a Picture to Upload"),
+                    PICK_IMAGE_REQUEST);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                mImageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImage() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+            picturePath = "ProfilPictures/"+ UUID.randomUUID().toString();
+            StorageReference ref = storageReference.child(picturePath);
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(ProfileActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(ProfileActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
 
     private void showPicture(String picturePath) {
-        StorageReference storageReference = FirebaseStorage.getInstance().getReference(picturePath);
+        StorageReference storageReference = storage.getReference(picturePath);
         ImageView imageView = findViewById(R.id.imageView);
         Glide.with(this /* context */).load(storageReference).into(imageView);
     }
@@ -79,9 +172,6 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     private void fillFormular(DocumentSnapshot document) {
-        String picturePath = (String) document.get("picturePath");
-        showPicture(picturePath);
-
         String userName = (String) document.get("name");
         final TextView nameTextfield = (TextView) findViewById(R.id.nameTextfield);
         nameTextfield.setText(userName);
@@ -102,11 +192,23 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             else
             {
                 ((TextView) findViewById(R.id.showDate)).setText(dateFormat.format(userbirth.toDate()));
+
+                btnChoose = (Button) findViewById(R.id.btnChoose);
+                mImageView = (ImageView) findViewById(R.id.imageView);
+                btnChoose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showFileChooser();
+                    }
+                });
             }
         }
         String userNumber = (String) document.get("phonenumber");
         final TextView numberTextfield = (TextView) findViewById(R.id.numberTextfield);
         numberTextfield.setText(userNumber);
+
+        String picturePath = (String) document.get("picturePath");
+        showPicture(picturePath);
     }
 
     @Override
@@ -136,6 +238,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     private void saveProfilChanges() {
+        uploadImage();
         final TextView dateField = (TextView) findViewById(R.id.showDate);
         final TextView nameTextield = (TextView) findViewById(R.id.nameTextfield);
         final TextView emailTextfield = (TextView) findViewById(R.id.emailTextfield);
@@ -146,9 +249,17 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         Timestamp birthday = new Timestamp(date);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("users").document(user.getUid()).update("birthday", birthday,"name", nameTextield.getText().toString(),"phonenumber", numberTextfield.getText().toString(), "email", emailTextfield.getText().toString());
+        if(filePath != null)
+        {
+            db.collection("users").document(user.getUid()).update("picturePath",picturePath,"birthday", birthday,"name", nameTextield.getText().toString(),"phonenumber", numberTextfield.getText().toString(), "email", emailTextfield.getText().toString());
+        }
+        else{
+            db.collection("users").document(user.getUid()).update("birthday", birthday,"name", nameTextield.getText().toString(),"phonenumber", numberTextfield.getText().toString(), "email", emailTextfield.getText().toString());
+        }
        // user.updateEmail(emailTextfield.getText().toString());
     }
+
+
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day) {
