f28860d7f25ac2f54d8a649a5f81e6aa4cd3e95b
Lea97 <lea.brock1@web.de>
2019-01-26 12:51:17 +0100

    Add a listener for the family dropdown element. So that the current family is stored in a static method and can be requested from the other activities.

--- app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FirebaseAuth mAuth;
     private ArrayList<String> items;
     private ArrayAdapter<String> adapter;
+    private static String currentFamily;
 
 
     @Override
@@ -168,6 +170,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
             adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
             dropdown.setAdapter(adapter);
+            dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    String family = dropdown.getSelectedItem().toString();
+                    setFamily(family);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {}
+            });
         }
+    }
+
+    private void setFamily(String family)
+    {
+        currentFamily = family;
+    }
+
+    public static String getFamily(){
+        return currentFamily;
     }
 }
