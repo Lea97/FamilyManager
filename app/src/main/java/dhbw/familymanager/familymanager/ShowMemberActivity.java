7e4f40facc9086d302d2c965d76574176a0b786c
Lea97 <lea.brock1@web.de>
2019-02-02 16:52:00 +0100

    Change the ListView that shows the members of a family so that the profile picture is displayed in addition to the e-mail address.

--- app/src/main/java/dhbw/familymanager/familymanager/ShowMemberActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/ShowMemberActivity.java
@@ -4,31 +4,35 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import dhbw.familymanager.familymanager.model.User;
 
 public class ShowMemberActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
     String family;
-    List<String> memberList;
     ListView simpleList;
+    List<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.members_list_layout);
 
-        memberList = new ArrayList<String>();
+        users = new ArrayList<User>();
         db = FirebaseFirestore.getInstance();
         family = MainActivity.getFamily();
         if(family != null)
@@ -39,8 +43,9 @@ public class ShowMemberActivity extends AppCompatActivity {
 
     private void addListAdapter() {
         simpleList = (ListView)findViewById(R.id.simpleListView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item_memberlist, R.id.memberTextView, memberList);
-        simpleList.setAdapter(arrayAdapter);
+        User[]usersArray = new User[users.size()];
+        MembersAdapter membersAdapter = new MembersAdapter(getApplicationContext(), users.toArray(usersArray), this);
+        simpleList.setAdapter(membersAdapter);
     }
 
     private void getFamilyMembers() {
@@ -65,11 +70,41 @@ public class ShowMemberActivity extends AppCompatActivity {
     }
 
     private void setMembers(DocumentSnapshot document) {
-        ArrayList<String> members = (ArrayList<String>) document.get("members");
+        final ArrayList<String> members = (ArrayList<String>) document.get("members");
 
-        for (String member:members) {
-            this.memberList.add(member);
+        Query query = db.collection("users");//.whereEqualTo("email",member);
+        query.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot querySnapshot) {
+                        List<DocumentSnapshot> documents = querySnapshot.getDocuments();
+                        for (DocumentSnapshot document : documents) {
+                            Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+
+                            String userMail = (String) document.get("email");
+                            if (members.contains(userMail)) {
+                                members.remove(userMail);
+                                User user = new User();
+                                user.setEmail(userMail);
+                                //user.setBirthday((Date) document.get("birthday"));
+                                //TODO
+                                user.setName((String) document.get("name"));
+                                user.setPhonenumber((String) document.get("phonenumber"));
+                                user.setPicturePath((String) document.get("picturePath"));
+
+                                users.add(user);
+                                Log.d("MEMBER HINZUGEFUEGT",userMail);
+                            }
+                        }
+                        for (String member : members) {
+                            Log.d("MEMBER",member);
+                            User user = new User();
+                            user.setEmail(member);
+                            user.setPicturePath("ProfilPictures/profile_picture.png");
+                            users.add(user);
+                        }
+                        addListAdapter();
+                    }
+                });
         }
-        addListAdapter();
-    }
 }
