0f8414d1777ee6d139a199cf6fe65e51f903c0a9
Anastasia Schwed <nshmyreva@gmail.com>
2019-03-10 14:37:31 +0100

    added methods to edit event(buggy)

--- app/src/main/java/dhbw/familymanager/familymanager/calendar/AddEventActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/calendar/AddEventActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -17,8 +18,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.sql.Time;
 import java.text.DateFormat;
@@ -28,6 +33,7 @@ import java.util.GregorianCalendar;
 import java.util.Random;
 
 import dhbw.familymanager.familymanager.DatePickerFragment;
+import dhbw.familymanager.familymanager.MainActivity;
 import dhbw.familymanager.familymanager.R;
 import dhbw.familymanager.familymanager.TimePickerFragment;
 import dhbw.familymanager.familymanager.controller.EventRepository;
@@ -44,6 +50,10 @@ class AddEventActivity extends AppCompatActivity implements DatePickerDialog.OnD
     private boolean startEvent=false;
     private CalendarView calendarView;
     private DatePickerDialog datePickerDialog;
+    private String eventId;
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
+    private Event event;
+    private String dbEntryId;
    // private Date startDate, endDate;
 
 
@@ -52,7 +62,10 @@ class AddEventActivity extends AppCompatActivity implements DatePickerDialog.OnD
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent=getIntent();
+        eventId=intent.getStringExtra("eventId");
         setContentView(R.layout.add_event);
+
         repository = EventRepository.getInstance(EventRepository.RepositoryMode.PRODUCTIVE);
         title=findViewById(R.id.eventTitle);
         location=findViewById(R.id.eventLocation);
@@ -78,79 +91,113 @@ class AddEventActivity extends AppCompatActivity implements DatePickerDialog.OnD
 
         eventStart=new GregorianCalendar();
         eventEnd=new GregorianCalendar();
+
+
         saveEventBtn = findViewById(R.id.saveEvent);
        saveEventBtn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               final Event e = new Event();
-               Random r=new Random();
-               e.setFamilyId("12345");
-               //e.setStart(new Date(2019, 1, 1, 14, 50, 0));
-               //e.setEnd(new Date(2019, 1, 1, 14, 55, 0));
-               e.setStart(new Date(eventStart.get(Calendar.YEAR)-1900, eventStart.get(Calendar.MONTH), eventStart.get(Calendar.DAY_OF_MONTH), eventStart.get(Calendar.HOUR_OF_DAY), eventStart.get(Calendar.MINUTE), 0));
-               e.setEnd(new Date(eventEnd.get(Calendar.YEAR)-1900, eventEnd.get(Calendar.MONTH), eventEnd.get(Calendar.DAY_OF_MONTH), eventEnd.get(Calendar.HOUR_OF_DAY), eventEnd.get(Calendar.MINUTE), 0));
-               e.setUid(FirebaseAuth.getInstance().getCurrentUser().getUid());
-               e.setTitle(title.getText().toString());
-               e.setId(r.nextLong());
-               e.setLocation(location.getText().toString());
+               //if(eventId!=null){
+                   //editEvent(eventId);
+              // }else{
+                   createNewEvent();
+              // }
 
 
-               Thread t=new Thread(new Runnable() {
-                   @Override
-                   public void run() {
-
-                       repository.storeEvent(e);
-                   }
-               });
-
-            t.start();
 
            }
        });}
 
+    private void createNewEvent() {
+        final Event e = new Event();
+        Random r=new Random();
+        e.setFamilyId(MainActivity.getFamily());
+        //e.setStart(new Date(2019, 1, 1, 14, 50, 0));
+        //e.setEnd(new Date(2019, 1, 1, 14, 55, 0));
+        e.setStart(new Date(eventStart.get(Calendar.YEAR)-1900, eventStart.get(Calendar.MONTH), eventStart.get(Calendar.DAY_OF_MONTH), eventStart.get(Calendar.HOUR_OF_DAY), eventStart.get(Calendar.MINUTE), 0));
+        e.setEnd(new Date(eventEnd.get(Calendar.YEAR)-1900, eventEnd.get(Calendar.MONTH), eventEnd.get(Calendar.DAY_OF_MONTH), eventEnd.get(Calendar.HOUR_OF_DAY), eventEnd.get(Calendar.MINUTE), 0));
+        e.setUid(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        e.setTitle(title.getText().toString());
+        e.setId(r.nextLong());
+        e.setLocation(location.getText().toString());
+
+
+        Thread t=new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                repository.storeEvent(e);
+            }
+        });
+
+        t.start();
+    }
+
+    private void editEvent(String eventId) {
+
+         db.collection("events").whereEqualTo("id", eventId).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                event = task.getResult().toObjects(Event.class).get(0);
+                dbEntryId = String.valueOf(event.getId());
+                final DocumentReference docRef = db.collection("events").document(dbEntryId);
+                docRef.update("title", title.getText().toString());
+                docRef.update("location", location.getText().toString());
+                docRef.update("start", new Date(eventStart.get(Calendar.YEAR) - 1900, eventStart.get(Calendar.MONTH), eventStart.get(Calendar.DAY_OF_MONTH), eventStart.get(Calendar.HOUR_OF_DAY), eventStart.get(Calendar.MINUTE), 0));
+                docRef.update("end", new Date(eventEnd.get(Calendar.YEAR) - 1900, eventEnd.get(Calendar.MONTH), eventEnd.get(Calendar.DAY_OF_MONTH), eventEnd.get(Calendar.HOUR_OF_DAY), eventEnd.get(Calendar.MINUTE), 0));
+
+
+            }
+
+
+
+        });}
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Calendar cal = new GregorianCalendar(year, month, dayOfMonth);
-        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
-        if(!startEvent) {
-            //if(eventStart.compareTo(Calendar.getInstance())>0)
-            eventStart.set(year, month, dayOfMonth);
+        {
+            Calendar cal = new GregorianCalendar(year, month, dayOfMonth);
+            final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+            if (!startEvent) {
+                //if(eventStart.compareTo(Calendar.getInstance())>0)
+                eventStart.set(year, month, dayOfMonth);
 
-            TimePickerFragment timePickerFragment=new TimePickerFragment();
-            timePickerFragment.show(getFragmentManager(), "datePicker");
+                TimePickerFragment timePickerFragment = new TimePickerFragment();
+                timePickerFragment.show(getFragmentManager(), "datePicker");
 
 
-        }
-        else{
+            } else {
 
-            eventEnd.set(year, month,dayOfMonth);
-            TimePickerFragment timePickerFragment=new TimePickerFragment();
-            timePickerFragment.show(getFragmentManager(), "datePicker");
-        }
+                eventEnd.set(year, month, dayOfMonth);
+                TimePickerFragment timePickerFragment = new TimePickerFragment();
+                timePickerFragment.show(getFragmentManager(), "datePicker");
+            }
 
 //        TimePicker mTimePicker = new TimePicker();
-        //datePickerDialog.show();
-        //((TextView) findViewById(R.id.showDate)).setText(dateFormat.format(cal.getTime()));
+            //datePickerDialog.show();
+            //((TextView) findViewById(R.id.showDate)).setText(dateFormat.format(cal.getTime()));
+
+        }
 
     }
 
-
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        if(!startEvent){
+        if (!startEvent) {
 
-        eventStart.set(Calendar.HOUR_OF_DAY,hourOfDay);
-        eventStart.set(Calendar.MINUTE,minute);
+            eventStart.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            eventStart.set(Calendar.MINUTE, minute);
 
-        //eventStart.set(hourOfDay, minute);
+            //eventStart.set(hourOfDay, minute);
 
-        startEvent = true;}
-        else{
-            eventEnd.set(Calendar.HOUR_OF_DAY,hourOfDay);
-            eventEnd.set(Calendar.MINUTE,minute);
+            startEvent = true;
+        } else {
+            eventEnd.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            eventEnd.set(Calendar.MINUTE, minute);
         }
 
 
     }
 }
+
+
---
+++ app/src/main/java/dhbw/familymanager/familymanager/calendar/EventDetailsActivity.java
@@ -0,0 +1,133 @@
+package dhbw.familymanager.familymanager.calendar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import dhbw.familymanager.familymanager.R;
+import dhbw.familymanager.familymanager.model.Event;
+
+public class EventDetailsActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText eventTitle, eventLocation, eventStart, eventEnd;
+    private Button edit, delete;
+    private Event event;
+    private String eventId, dbEntryId;
+
+
+    FirebaseFirestore db=FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_details);
+        eventTitle=findViewById(R.id.eventTitle1);
+        eventLocation=findViewById(R.id.eventLocation);
+        eventStart=findViewById(R.id.eventStart);
+        eventStart.setOnClickListener(this);
+        eventEnd=findViewById(R.id.eventEnd);
+        eventEnd.setOnClickListener(this);
+
+
+       Intent intent=getIntent();
+       eventId=intent.getStringExtra("eventId");
+       System.out.println(eventId);
+
+
+
+        db.collection("events").whereEqualTo("id", Long.parseLong(eventId)).get().
+        addOnCompleteListener((new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+
+                        event=task.getResult().toObjects(Event.class).get(0);
+                        //Log.d("TAG", document.getId() + " => " + document.getData());
+                        setEventData();
+                        dbEntryId=task.getResult().getDocuments().get(0).getId();
+
+
+                } else {
+                    Log.d("TAG", "Error getting documents: ", task.getException());
+                }
+            }
+
+        }));
+        delete=findViewById(R.id.deleteEvent);
+        delete.setOnClickListener(this);
+        edit=findViewById(R.id.editEvent);
+        edit.setOnClickListener(this);
+
+    }
+
+    private void setEventData() {
+
+        eventTitle.setText(event.getTitle());
+        eventLocation.setText(event.getLocation());
+        eventStart.setText(event.getStart().toString());
+        eventEnd.setText(event.getEnd().toString());
+       // eventId= String.valueOf(event.getId());
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case(R.id.deleteEvent):
+                deleteEvent(eventId);
+                break;
+            case(R.id.editEvent):
+                startEdit(eventId);
+                break;
+        }
+    }
+
+    private void startEdit(String eventId) {
+        Intent intent=new Intent(EventDetailsActivity.this, AddEventActivity.class);
+        intent.putExtra("eventId", eventId);
+        startActivity(intent);
+
+    }
+
+    private void deleteEvent(String eventId) {
+
+
+       db.collection("events").document(dbEntryId).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+           @Override
+           public void onSuccess(Void aVoid) {
+               Log.d("TAG", "DocumentSnapshot successfully deleted!");
+               System.out.println("Event mit id "+" deleted");
+               Toast.makeText(getApplicationContext(), " Event successfully deleted", Toast.LENGTH_SHORT).show();
+               startActivity(new Intent(EventDetailsActivity.this, CalendarActivity.class));
+           }
+       })
+               .addOnFailureListener(new OnFailureListener() {
+                   @Override
+                   public void onFailure(@NonNull Exception e) {
+                       Log.w("TAG", "Error deleting document", e);
+                   }
+               });
+
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(EventDetailsActivity.this, CalendarActivity.class));
+    }
+}
+
+
+
