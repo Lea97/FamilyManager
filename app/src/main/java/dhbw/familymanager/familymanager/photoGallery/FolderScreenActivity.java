8e4a112742b4ee8ab2888eb4553a2b7fbf2de6b6
Lea97 <lea.brock1@web.de>
2019-05-16 20:06:38 +0200

    Refactoring

--- app/src/main/java/dhbw/familymanager/familymanager/photoGallery/FolderScreenActivity.java
+++ app/src/main/java/dhbw/familymanager/familymanager/photoGallery/FolderScreenActivity.java
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
